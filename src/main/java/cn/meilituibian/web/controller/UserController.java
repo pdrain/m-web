152d3ccda64e002ec877eb891ab05d45db944ec4
pdrain <pdrain@yeah.net>
2018-08-11 22:02:33 +0800

    修改用户管理controller'

--- src/main/java/cn/meilituibian/web/controller/UserController.java
+++ src/main/java/cn/meilituibian/web/controller/UserController.java
@@ -2,8 +2,16 @@ package cn.meilituibian.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/admin/wx-user")
+@RequestMapping("/admin/user")
 public class UserController {
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public ModelAndView userList() {
+        ModelAndView view = new ModelAndView("userList");
+        view.setViewName("userList");
+        return view;
+    }
 }
